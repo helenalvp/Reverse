805d08241eb113fdd48af2b95bde5d41c04ff063
helenalvp <helenalvp@gmail.com>
2021-08-19 14:33:36 -0400

    initial commit to rewind

--- js/site.js
+++ js/site.js
@@ -1,5 +1,29 @@
-//Call Hello World
 
-function helloWorld(){
-    alert("Hello World")
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    if(userString !== ""){
+        let revString = reverseString(userString);
+        displayString(revString);  
+    } else{
+        alert("Please enter a string")
+    }
+}
+
+function reverseString(userStr){
+    let revStr = [];
+
+    for(let i = userStr.length - 1; i >= 0; i--){
+        revStr += userStr[i];
+    }
+
+    return revStr;
+}
+
+function displayString(reversedStr){
+
+    document.getElementById("msg").innerHTML = `Your string backwards is: ${reversedStr}`
+    document.getElementById("alert").classList.remove("invisible");
 }
